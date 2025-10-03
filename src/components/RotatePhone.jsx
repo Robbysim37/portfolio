4008047575187242a9a904de83c21aa4a2ebd39a
Robert Lewis <Robbysim37@gmail.com>
2025-10-02 21:09:15 -0500

    test

--- src/components/RotatePhone.jsx
+++ src/components/RotatePhone.jsx
@@ -3,7 +3,7 @@ import { FcRotateToLandscape } from "react-icons/fc";
  export default function RotatePhone() {
     return(
         <div className="flex flex-col justify-center items-center">
-            <p className="mb-8">Rotate your device to Landscape Mode</p>
+            <p className="mb-8 text-center">Rotate your device to Landscape Mode and make sure your sound is on!</p>
             <FcRotateToLandscape size={64}/>
         </div>
     )
